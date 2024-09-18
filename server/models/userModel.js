4d3e36c63e3e6cb9a216639170c0fe670268f78a
ilyaonop <ilya.onop@gmail.com>
2024-09-18 11:59:56 +0100

    GetUserById model implemented

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,7 +5,17 @@ exports.createUser = async (email, username, displayName, avatar) => {
     .from("users")
     .insert([{ email, username, display_name: displayName, avatar }]);
 
-    if (error) throw error;
-    return data
+  if (error) throw error;
+  return data;
 };
 
+exports.getUserById = async (userId) => {
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", userId)
+    .single();
+
+  if (error) throw error;
+  return data;
+};
