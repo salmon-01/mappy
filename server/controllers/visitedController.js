fdcf0746491f8cecb81f69e8beb7574ba828f2b1
ilyaonop <ilya.onop@gmail.com>
2024-09-18 13:30:28 +0100

    getVisited Controller implemented

--- server/controllers/visitedController.js
+++ server/controllers/visitedController.js
@@ -1,4 +1,4 @@
-const { addVisitedCountry } = require("../models/visitedModel");
+const { addVisitedCountry, getVisitedCountries } = require("../models/visitedModel");
 
 exports.addVisitedController = async (req, res) => {
   const { userId, countryCode } = req.body;
@@ -10,3 +10,14 @@ exports.addVisitedController = async (req, res) => {
     res.status(400).send({ error: error.message });
   }
 };
+
+exports.getVisitedController = async (req, res) => {
+  const {userId} = req.params;
+
+  try {
+    const countries = await getVisitedCountries(userId);
+    res.status(200).send(countries);
+  } catch (error) {
+    res.status(400).send({error: error.message})
+  }
+}
