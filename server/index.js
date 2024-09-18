9a487580629044cf5c5b0b88bd2a7f7f25f7a27e
ilyaonop <ilya.onop@gmail.com>
2024-09-18 14:35:15 +0100

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const userRoutes = require('./routes/userRoutes')
-const visitedRoutes = require('./routes/visitedRoutes')
-const wantToVisitRoutes = require('./routes/wantToVisitRoutes')
+const userRoutes = require("./routes/userRoutes");
+const visitedRoutes = require("./routes/visitedRoutes");
+const wantToVisitRoutes = require("./routes/wantToVisitRoutes");
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRoutes)
-app.use('/api/visited', visitedRoutes)
-app.use('/api/want-to-visit', wantToVisitRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/visited", visitedRoutes);
+app.use("/api/want-to-visit", wantToVisitRoutes);
 
 const PORT = 6543;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
