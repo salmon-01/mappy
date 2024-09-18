0e0973d38b9b3c790f0904ef27af5200fa9968e8
ilyaonop <ilya.onop@gmail.com>
2024-09-18 12:12:57 +0100

    getUser Controller implemented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,21 @@
-const { createUser } = require("../models/userModel");
+const { createUser, getUserById } = require("../models/userModel");
 
 exports.createUserController = async (req, res) => {
   const { email, username, displayName, avatar } = req.body;
-
   try {
     const user = await createUser(email, username, displayName, avatar);
-    res.status(201).json(user);
+    res.status(201).send(user);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).send({ error: error.message });
+  }
+};
+
+exports.getUserController = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await getUserById(userId);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
   }
 };
