847d429f2b50a749cfc9dca18897d009d80de697
ilyaonop <ilya.onop@gmail.com>
2024-09-20 00:02:21 +0100

    Auxilary files

--- Mappy/babel.config.js
+++ Mappy/babel.config.js
@@ -5,6 +5,8 @@ module.exports = function (api) {
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
 
-    plugins,
+    plugins: [
+      'react-native-reanimated/plugin', 
+    ],
   };
 };
