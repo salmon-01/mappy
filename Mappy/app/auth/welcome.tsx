fba80c6974e8f9139a83f5a72f993a65ee4d3e34
ilyaonop <ilya.onop@gmail.com>
2024-09-20 19:40:08 +0100

    Welcome screen update: welcome mock user

--- Mappy/app/auth/welcome.tsx
+++ Mappy/app/auth/welcome.tsx
@@ -3,15 +3,25 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import AvatarPicker from '~/components/AvatarPicker';
 import UserInputForm from '~/components/UserInputForm';
 import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const WelcomeScreen: React.FC = () => {
+const WelcomeScreen = () => {
   const [username, setUsername] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [avatar, setAvatar] = useState('');
   const router = useRouter();
 
-  const handleContinue = () => {
+  const handleContinue = async () => {
     if (username && displayName && avatar) {
+      const mockUserId = '0e494424-da07-4312-863b-ef833fd83a51';
+      const userData = {
+        id: mockUserId,
+        username,
+        displayName,
+        avatar,
+      };
+      await AsyncStorage.setItem('userData', JSON.stringify(userData));
+
       router.push('/home');
     } else {
       alert('Please complete all fields.');
