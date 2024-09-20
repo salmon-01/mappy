e922f9d72e0efdbc2d932ebdd03a8c659254cc22
ilyaonop <ilya.onop@gmail.com>
2024-09-20 19:36:45 +0100

    Splash screen update: conditional routing

--- Mappy/app/index.tsx
+++ Mappy/app/index.tsx
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import { isUserLoggedIn } from '~/services/authService';
 
 export default function SplashScreen() {
   const router = useRouter();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      router.replace('/auth/login');
-    }, 2000);
 
-    return () => clearTimeout(timer);
+    const checkLoginStatus = async () => {
+      const loggedIn = await isUserLoggedIn();
+      
+      if (loggedIn) {
+        router.push('/home'); // Redirect to home if logged in
+      } else {
+        router.push('/auth/login'); // Redirect to login if not logged in
+      }
+    };
+
+    checkLoginStatus();
   }, [router]);
 
   return (
