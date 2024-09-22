9f17f02d51b4221b2558fc84a2cb19c30589025a
ilyaonop <ilya.onop@gmail.com>
2024-09-22 21:57:59 +0100

    Layout updated: Context provider added

--- Mappy/app/(tabs)/_layout.tsx
+++ Mappy/app/(tabs)/_layout.tsx
@@ -1,37 +1,47 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { CountryProvider } from '~/context/CountryContext';
 
 export default function Layout() {
   return (
-    <Tabs
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    <CountryProvider>
+      <Tabs
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          if (route.name === 'home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'leaderboard') {
-            iconName = focused ? 'trophy' : 'trophy-outline';
-          } else if (route.name === 'dashboard') {
-            iconName = focused ? 'stats-chart' : 'stats-chart-outline';
-          }
+            if (route.name === 'home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'leaderboard') {
+              iconName = focused ? 'trophy' : 'trophy-outline';
+            } else if (route.name === 'dashboard') {
+              iconName = focused ? 'stats-chart' : 'stats-chart-outline';
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#007AFF',
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: {
-          height: 60,
-          paddingBottom: 0,
-        },
-      })}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Home',
-          headerShown: false,
-        }}
-      />
-    </Tabs>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#007AFF',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            height: 60,
+            paddingBottom: 0,
+          },
+        })}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: 'Home',
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            title: 'Dashboard',
+            headerShown: false,
+          }}
+        />
+      </Tabs>
+    </CountryProvider>
   );
 }
