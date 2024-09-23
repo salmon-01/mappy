d4c108eff588d208efd76f8344e74be8d4cf7c75
ilyaonop <ilya.onop@gmail.com>
2024-09-23 22:00:51 +0100

    Country List component update: styling added

--- Mappy/components/CountryList.tsx
+++ Mappy/components/CountryList.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
-import countries from '~/assets/countries.json'; 
-
+import countries from '~/assets/countries.json';
 
 interface CountryListProps {
   countries: string[];
-  getCountryDetails: (isoCode: string) => { name: string; flag: string; continent: string } | undefined;
-  showVisited: boolean; 
+  getCountryDetails: (
+    isoCode: string
+  ) => { name: string; flag: string; continent: string } | undefined;
+  showVisited: boolean;
 }
+
+const regionColors: Record<string, string> = {
+  Oceania: '#75C9DC',
+  'South America': '#9AD75C',
+  'North America': '#053D90',
+  Europe: '#F73AD8',
+  Asia: '#D7AD5C',
+  Antarctica: '#00BCD4',
+  Africa: '#981B1B',
+};
 
 // get total countries by continent from countries.json
 const getTotalCountriesByContinent = () => {
@@ -29,19 +40,24 @@ const getTotalCountriesByContinent = () => {
 // group countries by continent and calculate visited or want-to-visit counts
 const groupCountriesByContinent = (
   countries: string[],
-  getCountryDetails: (isoCode: string) => { name: string; flag: string; continent: string } | undefined
+  getCountryDetails: (
+    isoCode: string
+  ) => { name: string; flag: string; continent: string } | undefined
 ) => {
-  return countries.reduce((acc, code) => {
-    const country = getCountryDetails(code);
-    if (!country) return acc;
-    const { continent } = country;
-    if (!acc[continent]) {
-      acc[continent] = { countries: [], count: 0 };
-    }
-    acc[continent].countries.push(country);
-    acc[continent].count += 1; 
-    return acc;
-  }, {} as Record<string, { countries: { name: string; flag: string }[]; count: number }>);
+  return countries.reduce(
+    (acc, code) => {
+      const country = getCountryDetails(code);
+      if (!country) return acc;
+      const { continent } = country;
+      if (!acc[continent]) {
+        acc[continent] = { countries: [], count: 0 };
+      }
+      acc[continent].countries.push(country);
+      acc[continent].count += 1;
+      return acc;
+    },
+    {} as Record<string, { countries: { name: string; flag: string }[]; count: number }>
+  );
 };
 
 const CountryList: React.FC<CountryListProps> = ({ countries, getCountryDetails, showVisited }) => {
@@ -53,15 +69,27 @@ const CountryList: React.FC<CountryListProps> = ({ countries, getCountryDetails,
       data={Object.entries(groupedCountries)}
       keyExtractor={([continent]) => continent}
       renderItem={({ item: [continent, { countries: countryList, count }] }) => (
+
         <View style={styles.continentSection}>
-          <Text style={styles.continentHeader}>
-            {continent} {showVisited ? `${count}/${totalCountriesByContinent[continent]}` : `${count}`}
-          </Text>
+
+          <View style={styles.regionHeaderContainer}>
+            <View style={[styles.regionBadge, { backgroundColor: regionColors[continent] }]}>
+              <Text style={styles.regionText}>{continent}</Text>
+            </View>
+
+            <Text style={styles.countryCount}>
+              {showVisited ? `${count}/${totalCountriesByContinent[continent]}` : `${count}`}
+            </Text>
+          </View>
+
+          <View style={styles.separator} />
+
           {countryList.map((country, index) => (
             <View key={index} style={styles.countryRow}>
               <Text style={styles.countryName}>{country.name}</Text>
             </View>
           ))}
+          
         </View>
       )}
     />
@@ -72,20 +100,40 @@ const styles = StyleSheet.create({
   continentSection: {
     marginBottom: 20,
   },
-  continentHeader: {
+  regionHeaderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  regionBadge: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    borderRadius: 15,
+  },
+  regionText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+  countryCount: {
     fontSize: 18,
     fontWeight: 'bold',
+    color: '#333',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#E0E0E0',
     marginBottom: 10,
   },
   countryRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
     paddingVertical: 5,
   },
   countryName: {
-    fontSize: 16,
+    fontSize: 17,
+    color: '#333333',
+    fontWeight: '500',
   },
 });
-
 
 export default CountryList;
