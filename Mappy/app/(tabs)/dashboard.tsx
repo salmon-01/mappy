6aad2e4ba04a040a8f65aa1bb770c08a06d9ec86
ilyaonop <ilya.onop@gmail.com>
2024-09-23 22:10:49 +0100

    Dashboard screen update: minor refactor

--- Mappy/app/(tabs)/dashboard.tsx
+++ Mappy/app/(tabs)/dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import countries from '~/assets/countries.json';
-import { CountryContext } from '~/context/CountryContext'; 
+import { CountryContext } from '~/context/CountryContext';
 import UserProfile from '~/components/UserProfile';
 import Stats from '~/components/Stats';
 import CountryBottomSheet from '~/components/CountryBottomSheet';
@@ -9,15 +9,19 @@ import SettingsBottomSheet from '~/components/SettingsBottomSheet';
 
 import avatar from '~/assets/avatars/avatar1.png';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useRouter } from 'expo-router';
 
 const Dashboard = () => {
-  const { visitedCountries, wishlistCountries } = useContext(CountryContext)!; 
+  const router = useRouter();
+  const { visitedCountries, wishlistCountries } = useContext(CountryContext)!;
   const [showVisited, setShowVisited] = useState(true);
   const [isSettingsVisible, setSettingsVisible] = useState(false);
 
   // DB country_code to countries.json country details
   const getCountryDetails = (isoCode: string): { name: string; continent: string } | undefined => {
-    const country = countries.find((c: { iso_3166_1_alpha_3: string }) => c.iso_3166_1_alpha_3 === isoCode);
+    const country = countries.find(
+      (c: { iso_3166_1_alpha_3: string }) => c.iso_3166_1_alpha_3 === isoCode
+    );
     return country ? { name: country.name, continent: country.continent || 'Unknown' } : undefined;
   };
 
@@ -26,23 +30,26 @@ const Dashboard = () => {
   const regionsVisited = new Set(visitedCountries.map((c) => getCountryDetails(c)?.continent)).size;
 
   const totalWishlistCountries = wishlistCountries.length;
-  const favoriteRegion =
-    Object.entries(
-      wishlistCountries.reduce(
-        (acc, code) => {
-          const country = getCountryDetails(code);
-          const { continent } = country || {};
-          if (continent) acc[continent] = (acc[continent] || 0) + 1;
-          return acc;
-        },
-        {} as Record<string, number>
-      )
-    ).sort((a, b) => b[1] - a[1])[0]?.[0] || 'Unknown Region'; // region with the most "want to visit" countries
+  const favoriteRegion = Object.entries(
+    wishlistCountries.reduce(
+      (acc, code) => {
+        const country = getCountryDetails(code);
+        const { continent } = country || {};
+        if (continent) acc[continent] = (acc[continent] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>
+    )
+  ).sort((a, b) => b[1] - a[1])[0]?.[0]; // region with the most "want to visit" countries
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <UserProfile username="salmon-01" avatar={avatar} onPress={() => setSettingsVisible(true)} />
+        <UserProfile
+          username="Test User 1"
+          avatar={avatar}
+          onPress={() => setSettingsVisible(true)}
+        />
         {showVisited ? (
           <Stats
             totalVisited={totalVisitedCountries}
@@ -62,10 +69,10 @@ const Dashboard = () => {
         />
 
         <SettingsBottomSheet
-          isVisible={isSettingsVisible} 
-          onClose={() => setSettingsVisible(false)} 
+          isVisible={isSettingsVisible}
+          onClose={() => setSettingsVisible(false)}
           onClearMap={() => console.log('Clear map pressed')}
-          onLogout={() => console.log('Logout pressed')}
+          onLogout={() => router.push('/auth/login')}
           onDeleteAccount={() => console.log('Delete account pressed')}
         />
       </View>
@@ -77,7 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#5EBDE5',
   },
 });
 
