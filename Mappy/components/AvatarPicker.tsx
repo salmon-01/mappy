75f6d28fff25db20cd1246958a9f92e2ba1b3ddc
ilyaonop <ilya.onop@gmail.com>
2024-09-24 13:58:34 +0100

    Avatar Picker component update: styling added

--- Mappy/components/AvatarPicker.tsx
+++ Mappy/components/AvatarPicker.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet, Text } from 'react-native';
 
 const avatars: { [key: string]: any } = {
   avatar1: require('../assets/avatars/avatar1.png'),
+  avatar2: require('../assets/avatars/avatar2.png'),
+  avatar3: require('../assets/avatars/avatar3.png'),
+  avatar4: require('../assets/avatars/avatar4.png'),
+  avatar5: require('../assets/avatars/avatar5.png'),
 };
 
 interface AvatarPickerProps {
@@ -13,32 +17,48 @@ interface AvatarPickerProps {
 const AvatarPicker: React.FC<AvatarPickerProps> = ({ selectedAvatar, setAvatar }) => {
   return (
     <View style={styles.container}>
-      {Object.keys(avatars).map((avatarKey, index) => (
-        <TouchableOpacity key={index} onPress={() => setAvatar(avatarKey)}>
-          <Image
-            source={avatars[avatarKey]}
-            style={[styles.avatar, selectedAvatar === avatarKey && styles.selectedAvatar]}
-          />
-        </TouchableOpacity>
-      ))}
+      <Text style={styles.text}>Choose your Mappy Avatar</Text>
+      <View style={styles.avatarsContainer}>
+        {Object.keys(avatars).map((avatarKey, index) => (
+          <TouchableOpacity key={index} onPress={() => setAvatar(avatarKey)}>
+            <Image
+              source={avatars[avatarKey]}
+              style={[styles.avatar, selectedAvatar === avatarKey && styles.selectedAvatar]}
+            />
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  avatarsContainer: {
+    flexDirection: 'row', 
+    flexWrap: 'wrap', 
+    justifyContent: 'flex-start', 
+    gap: 1, 
+    marginTop: 8,
+    marginBottom: 16
   },
   avatar: {
-    width: 50,
-    height: 50,
-    margin: 10,
+    width: 75,
+    height: 75,
     opacity: 0.75,
+    marginRight: 2,
+    marginBottom: 10,
   },
   selectedAvatar: {
     opacity: 1,
+  },
+  text: {
+    color: '#333333',
+    fontWeight: '600',
+    marginBottom: 6,
+    fontSize: 16
   },
 });
 
